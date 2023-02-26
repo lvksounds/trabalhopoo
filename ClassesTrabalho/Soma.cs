8003a4fd527b83aa9e3b205038d7a9d731ecda94
Lucas <lvksounds@gmail.com>
2023-02-26 11:18:38 -0300

    initial commit

---
+++ ClassesTrabalho/Soma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTrabalho
+{
+    internal class Soma
+    {
+        public static string CalculaSoma(int x, int y)
+        {
+            return $"A soma de {x} com {y} é: {x + y}";
+        }
+    }
+}
